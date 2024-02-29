4173057e407ab50c62fd960064b87e8a9725802d
Viktor Petrov <126717931+viktorpetrov1997@users.noreply.github.com>
2024-02-29 23:54:12 +0200

    Update exercise

--- 2. Arrays and Nested Arrays/EqualNeighbors.js
+++ 2. Arrays and Nested Arrays/EqualNeighbors.js
@@ -24,8 +24,9 @@ function equalNeighborsCount(matrix)
     return neighbors;
 }
 
-let matrix = [['2', '2', '5', '7', '4'],
+let matrix = [['2', '3', '4', '7', '0'],
              ['4', '0', '5', '3', '4'],
-             ['2', '5', '5', '4', '2']];
+             ['2', '3', '5', '4', '2'],
+             ['9', '8', '7', '5', '4']];
 
 console.log(equalNeighborsCount(matrix));
