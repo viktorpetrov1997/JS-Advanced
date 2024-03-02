26c2bb9f76afb419d0a5678a3f46b3b7fce0a67f
Viktor Petrov <126717931+viktorpetrov1997@users.noreply.github.com>
2024-03-02 02:18:20 +0200

    Add exercises

---
+++ 3. Objects and Composition/CityRecord.js
@@ -0,0 +1,25 @@
+function createRecord(name, population, treasury) 
+{
+    const city = {
+        name: name,
+        population: population,
+        treasury: treasury
+        };
+
+    return city;
+}
+
+/*
+
+function createRecord(name, population, treasury) 
+{
+const city = {};
+city.name = name;
+city.population = population;
+city.treasury = treasury;
+return city;
+}
+
+*/
+
+
--- 3. Objects and Composition/CityTaxes.js
+++ 3. Objects and Composition/CityTaxes.js
@@ -1,24 +1,25 @@
-function cityTaxes(name, population, treasury)
+function cityTaxes(name, population, treasury) 
 {
-    return {
-        name, 
-        population, 
-        treasury,
-        taxRate : 10,
-    collectTaxes() 
-    {
-        this.treasury += this.population * this.taxRate;
-    },
-    applyGrowth(percent)
-    {
-        this.population += Math.floor(this.population * percent / 100);
-    },
-    applyRecession(percent)
-    {
-        this.treasury -= Math.floor(this.treasury * percent / 100);
-    },
-    };
+    const city = {
+        name: name,
+        population: population,
+        treasury: treasury,
+        taxRate: 10,
+        collectTaxes()
+        {
+            this.treasury += this.population * this.taxRate;
+        },
+        applyGrowth(percentage)
+        {
+            this.population += Math.floor(this.population * (percentage / 100));
+        },
+        applyRecession(percentage)
+        {
+            this.treasury -= Math.floor(this.treasury * (percentage / 100));
+        }
+        };
+
+    return city;
 }
 
-const city = cityTaxes('Tortuga',7000,15000);
-console.log(city);
+console.log(cityTaxes('Tortuga',7000,15000));
